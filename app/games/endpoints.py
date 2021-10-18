a198d244d234752429d9f3934cdf17a447464f11
Valentino Beorda <valentinobeorda@outlook.com>
2021-10-18 12:47:53 -0300

    Cambio en nombre de variables y actualización de respuesta HTPP

--- app/games/endpoints.py
+++ app/games/endpoints.py
@@ -7,12 +7,13 @@ router = APIRouter(prefix='/games')
 @router.get("/{idG}/dice/{idP}")
 async def getDice(idG:int, idP:int, response:Response):
     with db_session:
-        g = Game.get(id=idG)
-        p = Player.get(id=idP)
-        if (p is None or g is None):
+        game = Game.get(id=idG)
+        player = Player.get(id=idP)
+        if (player is None or game is None):
             response.status_code = status.HTTP_404_NOT_FOUND
             return {'Error' : 'Jugador o partida no existentes'}
-        elif (g.currentTurn == p.turnOrder):
+        elif (game.currentTurn == player.turnOrder):
+            response.status_code = status.HTTP_200_OK
             return randint(1,6)
         else: 
             response.status_code = status.HTTP_403_FORBIDDEN
