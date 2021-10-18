1a0824783bcfa114ad87a4419ccbb73b1fa93460
alvie97 <alvie97m@gmail.com>
2021-10-18 00:27:59 -0300

    agrega endpoint de partida

--- app/api.py
+++ app/api.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from app.models import db
+from app.games.endpoints import router as gameRouter
 
 app = FastAPI()
+app.include_router(gameRouter)
 
 db.bind('sqlite', 'DataBase.sqlite', create_db=True)
 db.generate_mapping(create_tables=True)
---
+++ app/games/schemas.py
@@ -0,0 +1,5 @@
+from pydantic import BaseModel
+
+class CreateGameSchema(BaseModel):
+    gameName: str
+    hostNickname: str
