8066b55dd4413b7583d599e0ba2c951b6bb6ce4b
Valentino Beorda <valentinobeorda@outlook.com>
2021-10-18 20:59:44 -0300

    Actualización de la lógica del if

--- app/games/endpoints.py
+++ app/games/endpoints.py
@@ -9,11 +9,13 @@ async def getDice(idG:int, idP:int, response:Response):
     with db_session:
         game = Game.get(id=idG)
         player = Player.get(id=idP)
-        if (player is None or game is None):
+        if (game is None):
             response.status_code = status.HTTP_404_NOT_FOUND
-            return {'Error' : 'Jugador o partida no existentes'}
+            return {'Error' : 'Partida no existente'}
+        elif (player is None):
+            response.status_code = status.HTTP_404_NOT_FOUND
+            return {'Error' : 'Jugador no existente'}
         elif (game.currentTurn == player.turnOrder):
-            response.status_code = status.HTTP_200_OK
             return randint(1,6)
         else: 
             response.status_code = status.HTTP_403_FORBIDDEN
