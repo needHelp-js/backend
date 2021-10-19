3c5b0f92ff4873ffa32bbf4b251d5ea795a8ea6f
alvie97 <alvie97m@gmail.com>
2021-10-18 21:27:23 -0300

    agrega busqueda de registros por atributos

--- tests/test_games.py
+++ tests/test_games.py
@@ -5,14 +5,17 @@ from app.models import Player, Game
 
 
 def test_createGame_success(client):
+    gameName = 'Game test'
+    hostNickname = 'test_host_nickname'
+
     response = client.post('/games',
                            json={
-                               'gameName': 'Game test',
-                               'hostNickname': 'test_host_nickname'
+                               'gameName': gameName,
+                               'hostNickname': hostNickname
                            })
 
     assert response.status_code == status.HTTP_201_CREATED
     with db_session:
-        game = Game.select().first()
-        player = Player.select().first()
+        game = Game.get(name=gameName)
+        player = Player.get(nickname=hostNickname)
     assert response.json() == {'idPartida': game.id, 'idHost': player.id}
