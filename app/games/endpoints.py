5f5ffb98876d99c0da91f67e77eddcae86a86e86
alvie97 <alvie97m@gmail.com>
2021-10-19 23:24:36 -0300

    da formato al codigo

--- app/games/endpoints.py
+++ app/games/endpoints.py
@@ -3,18 +3,17 @@ from app.models import Game, Player
 from .schemas import CreateGameSchema
 from pony.orm import db_session
 
-router = APIRouter(prefix='/games')
+router = APIRouter(prefix="/games")
 
 
-@router.post('', status_code=status.HTTP_201_CREATED)
+@router.post("", status_code=status.HTTP_201_CREATED)
 def createGame(gameCreationData: CreateGameSchema, response: Response):
     with db_session:
         if Game.exists(name=gameCreationData.gameName):
             response.status_code = status.HTTP_400_BAD_REQUEST
-            return {'Error': f'Partida {gameCreationData.gameName} ya existe'}
+            return {"Error": f"Partida {gameCreationData.gameName} ya existe"}
 
-        hostPlayer = Player(nickname=gameCreationData.hostNickname,
-                            turnOrder=0)
+        hostPlayer = Player(nickname=gameCreationData.hostNickname, turnOrder=0)
         newGame = Game(name=gameCreationData.gameName, host=hostPlayer)
 
-    return {'idPartida': newGame.id, 'idHost': hostPlayer.id}
+    return {"idPartida": newGame.id, "idHost": hostPlayer.id}
--- tests/test_games.py
+++ tests/test_games.py
@@ -5,33 +5,30 @@ from app.models import Player, Game
 
 
 def test_createGame_success(client):
-    gameName = 'Game test'
-    hostNickname = 'test_host_nickname'
+    gameName = "Game test"
+    hostNickname = "test_host_nickname"
 
-    response = client.post('/games',
-                           json={
-                               'gameName': gameName,
-                               'hostNickname': hostNickname
-                           })
+    response = client.post(
+        "/games", json={"gameName": gameName, "hostNickname": hostNickname}
+    )
 
     assert response.status_code == status.HTTP_201_CREATED
     with db_session:
         game = Game.get(name=gameName)
         player = Player.get(nickname=hostNickname)
-    assert response.json() == {'idPartida': game.id, 'idHost': player.id}
+    assert response.json() == {"idPartida": game.id, "idHost": player.id}
+
 
 def test_createGame_failure_game_already_exists(client):
-    gameName = 'Game test'
-    hostNickname = 'test_host_nickname'
+    gameName = "Game test"
+    hostNickname = "test_host_nickname"
 
     with db_session:
         player = Player(nickname=hostNickname, turnOrder=0)
         Game(name=gameName, host=player)
 
-    response = client.post('/games',
-                           json={
-                               'gameName': gameName,
-                               'hostNickname': hostNickname
-                           })
+    response = client.post(
+        "/games", json={"gameName": gameName, "hostNickname": hostNickname}
+    )
     assert response.status_code == status.HTTP_400_BAD_REQUEST
-    assert 'Error' in response.json()
+    assert "Error" in response.json()
