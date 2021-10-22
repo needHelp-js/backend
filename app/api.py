8619f5e353426d43108326b9cdf01162db135790
Valentino Cerutti <valentinocerutti@gmail.com>
2021-10-22 00:11:35 -0300

    Agrega endopoint de listar partidas

--- app/api.py
+++ app/api.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
 from app.models import db
+from app.games.endpoints import router as gamesRouter
 
 app = FastAPI()
 
 db.bind('sqlite', 'DataBase.sqlite', create_db=True)
 db.generate_mapping(create_tables=True)
 
-
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+app.include_router(gamesRouter)
---
+++ app/games/endpoints.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter, Response
+from app.models import *
+
+router = APIRouter(prefix='/games')
+
+@router.get("/")
+async def getGames():
+    with db_session:
+        games = Game.select(lambda p: len(p.players) < 6 and p.started == False)[:]
+
+        gamesList = []
+
+        for game in games:
+            gameDict = game.to_dict(['id', 'name'])
+            gameDict.update(playerCount = len(game.players))
+
+            gamesList.append(gameDict)
+
+        return gamesList
