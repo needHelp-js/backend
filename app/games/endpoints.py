0730f99472ac0a9c533df6dba3c87a8daf0c1781
Valentino Cerutti <valentinocerutti@gmail.com>
2021-10-24 20:07:07 -0300

    Agrega schema de AvailableGame

--- app/games/endpoints.py
+++ app/games/endpoints.py
@@ -1,10 +1,12 @@
+from typing import List
 from fastapi import APIRouter, Response
+from app.games.schemas import AvailableGameSchema
 from app.models import *
 
 router = APIRouter(prefix="/games")
 
 
-@router.get("/")
+@router.get("/", response_model=List[AvailableGameSchema])
 async def getGames():
     with db_session:
         games = Game.select(lambda p: len(p.players) < 6 and p.started == False)[:]
