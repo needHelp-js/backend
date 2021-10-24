e303c7e4cedbf5e0a7d2436e8286d93a02c24861
Valentino Cerutti <valentinocerutti@gmail.com>
2021-10-24 18:39:02 -0300

    Agrega tests de listar partidas

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,8 @@
+from pony.orm.core import db_session, flush
 import pytest
 import os
 from fastapi.testclient import TestClient
+from app.models import Game, Player
 
 from config import Config
 
@@ -29,3 +31,29 @@ def app():
 @pytest.fixture
 def client(app):
     return TestClient(app)
+
+@pytest.fixture
+def data():
+    with db_session:
+        players = []
+        for i in range(21):
+            players.append(Player(id=i, nickname=f'p{i}'))
+        
+        # Only one player
+        g1 = Game(id=1, name='g1', host=players[0])
+        # Six players
+        g2 = Game(id=2, name='g2', host=players[1])
+        # Four players, and started
+        g3 = Game(id=3, name='g3', host=players[7], started=True)
+        # Six players and started
+        g4 = Game(id=4, name='g4', host=players[11], started=True)
+        # Four players, not started
+        g5 = Game(id=5, name='g5', host=players[17])
+        
+        flush()
+
+        g1.players.add(players[0])     # 1
+        g2.players.add(players[1:7])   # 6
+        g3.players.add(players[7:11])  # 4
+        g4.players.add(players[11:17]) # 6
+        g5.players.add(players[17:21]) # 4
---
+++ tests/test_games.py
@@ -0,0 +1,29 @@
+from fastapi import status
+from pony.orm import db_session
+
+from app.models import Player, Game
+
+
+def test_getGames_success(client, data):
+
+    response = client.get("/games")
+
+    assert response.status_code == 200
+    assert response.json() == [
+        {
+            "id": 1,
+            "name": "g1",
+            "playerCount": 1
+        },{
+            "id": 5,
+            "name": "g5",
+            "playerCount": 4
+        }
+    ]
+
+def test_getGames_no_games(client):
+
+    response = client.get("/games")
+
+    assert response.status_code == 200
+    assert response.json() == []
