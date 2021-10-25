1344065a964bc69d0a0ef6e8847afaa8c9ec955a
alvie97 <alvie97m@gmail.com>
2021-10-24 21:02:25 -0300

    formatea codigo

--- tests/test_game_connection_manager.py
+++ tests/test_game_connection_manager.py
@@ -1,93 +1,100 @@
 import pytest
 from fastapi.testclient import TestClient
 from starlette.websockets import WebSocket
-from app.games.exceptions import GameConnectionDoesNotExist, PlayerAlreadyConnected, PlayerNotConnected
+from app.games.exceptions import (
+    GameConnectionDoesNotExist,
+    PlayerAlreadyConnected,
+    PlayerNotConnected,
+)
+
 
 def test_connectPlayerToGame_success(app, gameManager):
-
-    @app.websocket('/wsTestRoute')
+    @app.websocket("/wsTestRoute")
     async def wsTestRoute(websocket: WebSocket):
         await websocket.accept()
-    
+
     client = TestClient(app)
 
-    with client.websocket_connect('/wsTestRoute') as websocket:
+    with client.websocket_connect("/wsTestRoute") as websocket:
         gameManager.createGameConnection(1)
         gameManager.connectPlayerToGame(1, 1, websocket)
 
         assert 1 in gameManager._games[1]
         assert websocket == gameManager._games[1][1]
 
-def test_connectPlayerToGame_raiseExceptions(app, gameManager):
 
-    @app.websocket('/wsTestRoute')
+def test_connectPlayerToGame_raiseExceptions(app, gameManager):
+    @app.websocket("/wsTestRoute")
     async def wsTestRoute(websocket: WebSocket):
         await websocket.accept()
 
     client = TestClient(app)
 
-    with client.websocket_connect('/wsTestRoute') as websocket:
+    with client.websocket_connect("/wsTestRoute") as websocket:
         with pytest.raises(GameConnectionDoesNotExist):
             gameManager.connectPlayerToGame(1, 1, websocket)
-    
+
     gameManager.createGameConnection(1)
 
-    with client.websocket_connect('/wsTestRoute') as websocket:
+    with client.websocket_connect("/wsTestRoute") as websocket:
         gameManager.connectPlayerToGame(1, 1, websocket)
-    
-    with client.websocket_connect('/wsTestRoute') as websocket:
+
+    with client.websocket_connect("/wsTestRoute") as websocket:
         with pytest.raises(PlayerAlreadyConnected):
             gameManager.connectPlayerToGame(1, 1, websocket)
 
+
 def test_disconnectPlayerFromGame_success(app, gameManager):
     gameManager.createGameConnection(1)
-    
-    @app.websocket('/wsTestRoute')
+
+    @app.websocket("/wsTestRoute")
     async def wsTestRoute(websocket: WebSocket):
         await websocket.accept()
 
-    
     client = TestClient(app)
 
-    with client.websocket_connect('/wsTestRoute') as websocket:
+    with client.websocket_connect("/wsTestRoute") as websocket:
         gameManager.connectPlayerToGame(1, 1, websocket)
         gameManager.connectPlayerToGame(1, 2, websocket)
-    
+
     gameManager.disconnectPlayerFromGame(1, 1)
-    
+
     assert 1 not in gameManager._games[1]
     assert 2 in gameManager._games[1]
-    
+
     gameManager.disconnectPlayerFromGame(1, 2)
 
     assert 1 not in gameManager._games
 
+
 def test_disconnectPlayerFromGame_raiseExceptions(gameManager):
-    
+
     with pytest.raises(GameConnectionDoesNotExist):
         gameManager.disconnectPlayerFromGame(1, 1)
-    
+
     gameManager.createGameConnection(1)
 
     with pytest.raises(PlayerNotConnected):
         gameManager.disconnectPlayerFromGame(1, 2)
 
+
 def test_sendMessageToPlayer_success(app, gameManager):
-    @app.websocket('/wsTestRoute')
+    @app.websocket("/wsTestRoute")
     async def wsTestRoute(websocket: WebSocket):
         await websocket.accept()
         gameManager.createGameConnection(1)
         gameManager.connectPlayerToGame(1, 1, websocket)
-        await gameManager.sendToPlayer(1, 1, {"msg": "data"})   
-    
+        await gameManager.sendToPlayer(1, 1, {"msg": "data"})
+
     client = TestClient(app)
 
-    with client.websocket_connect('/wsTestRoute') as websocket:
+    with client.websocket_connect("/wsTestRoute") as websocket:
         data = websocket.receive_json()
         assert {"msg": "data"} == data
-    
+
+
 def test_sendMessageToPlayer_raiseExceptions(app, gameManager):
-    @app.websocket('/wsTestRoute')
+    @app.websocket("/wsTestRoute")
     async def wsTestRoute(websocket: WebSocket):
         await websocket.accept()
         gameManager.createGameConnection(1)
@@ -99,33 +106,34 @@ def test_sendMessageToPlayer_raiseExceptions(app, gameManager):
 
     client = TestClient(app)
 
-    with client.websocket_connect('/wsTestRoute'):
+    with client.websocket_connect("/wsTestRoute"):
         pass
+
 
 def test_broadcastToGame_success(app, gameManager):
     gameManager.createGameConnection(1)
 
-    @app.websocket('/wsTestRoute')
+    @app.websocket("/wsTestRoute")
     async def wsTestRoute(websocket: WebSocket):
         await websocket.accept()
         gameManager.connectPlayerToGame(1, 1, websocket)
-        await gameManager.broadcastToGame(1, {"msg": "data"})   
-    
+        await gameManager.broadcastToGame(1, {"msg": "data"})
+
     client = TestClient(app)
 
-    with client.websocket_connect('/wsTestRoute') as websocket:
+    with client.websocket_connect("/wsTestRoute") as websocket:
         data = websocket.receive_json()
         assert {"msg": "data"} == data
 
-def test_broadcastToGame_raiseExcpetions(app, gameManager):
 
-    @app.websocket('/wsTestRoute')
+def test_broadcastToGame_raiseExcpetions(app, gameManager):
+    @app.websocket("/wsTestRoute")
     async def wsTestRoute(websocket: WebSocket):
         await websocket.accept()
         with pytest.raises(GameConnectionDoesNotExist):
-            await gameManager.broadcastToGame(1, {"msg": "data"})   
-    
+            await gameManager.broadcastToGame(1, {"msg": "data"})
+
     client = TestClient(app)
 
-    with client.websocket_connect('/wsTestRoute'):
+    with client.websocket_connect("/wsTestRoute"):
         pass
