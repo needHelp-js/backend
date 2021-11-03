5f8bcbc73026e3397c9367cb926c870275cc65ee
alvie97 <alvie97m@gmail.com>
2021-11-03 20:20:48 -0300

    formato

--- tests/test_cards_model.py
+++ tests/test_cards_model.py
@@ -1,13 +1,15 @@
 from app.models import checkCardName, checkCardType
 from app.enums import CardType, MonstersNames, RoomName, VictimsNames
 
+
 def test_checkCardType():
 
     assert checkCardType("something different") == False
     assert checkCardType(CardType.MONSTER.value)
 
+
 def test_checkCardName():
-    
+
     assert checkCardName("something different") == False
     assert checkCardName(VictimsNames.CONDE.value)
     assert checkCardName(MonstersNames.DRACULA.value)
