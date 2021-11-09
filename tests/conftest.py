4678cd13bcf2fc8099f1062fad565f4d2e9a49c1
Valentino Cerutti <valentinocerutti@gmail.com>
2021-11-09 01:12:41 -0300

    Agrega tests de los mixin

--- tests/conftest.py
+++ tests/conftest.py
@@ -135,3 +135,31 @@ def dataCards():
         flush()
 
         g1.players.add([p1, p2])
+
+@pytest.fixture
+def dataSuspect():
+    with db_session:
+        p1 = Player(id=1, nickname="p1", turnOrder=1)
+        p2 = Player(id=2, nickname="p2", turnOrder=2)
+        p3 = Player(id=3, nickname="p3", turnOrder=3)
+        p4 = Player(id=4, nickname="p4", turnOrder=4)
+        g1 = Game(id=1, name="g1", host=p1, currentTurn=1)
+
+        flush()
+
+        g1.players.add([p1,p2,p3,p4])
+
+        c1 = Card(id=1, type="victima", name="Conde", game=g1)
+        c2 = Card(id=2, type="victima", name="Condesa", game=g1)
+        c3 = Card(id=3, type="monstruo", name="Drácula", game=g1)
+        c4 = Card(id=4, type="monstruo", name="Hombre Lobo", game=g1)
+        c5 = Card(id=5, type="recinto", name="Cochera", game=g1)
+        c6 = Card(id=6, type="recinto", name="Panteón", game=g1)
+
+        c7 = Card(id=7, type="victima", name="Mayordomo", game=g1)
+        c8 = Card(id=8, type="victima", name="Doncella", game=g1)
+
+        p1.cards.add(c8)
+        p2.cards.add([c2,c3,c5])
+        p3.cards.add(c1)
+        p4.cards.add(c6)
--- tests/test_mixins.py
+++ tests/test_mixins.py
@@ -1,6 +1,5 @@
 from pony.orm import db_session
-from app.mixins import GameMixin
-from app.models import Game
+from app.models import Card, Game, Player
 
 
 def test_startGame(app, beginGameData):
@@ -41,3 +40,77 @@ def test_setPlayerTurnOrder_success(app, data):
     assert 6 in playerTurns
 
     assert len(playerTurns) == len(set(playerTurns))
+
+def test_filterCards_noCardsMatching(app, dataSuspect):
+
+    with db_session:
+        p1 = Player[1]
+        finalCards = p1.filterCards(["Hombre Lobo", "Gato", "Perro"])[:]
+
+        assert finalCards == []
+
+def test_filterCards_success(app, dataSuspect):
+
+    with db_session:
+        p2 = Player[2]
+        finalCards = p2.filterCards(["Condesa", "Gato", "Perro"])[:]
+
+        assert [c.name for c in finalCards] == ["Condesa"]
+
+def test_filterCards_multipleSuccess(app, dataSuspect):
+
+    with db_session:
+        p2 = Player[2]
+        finalCards = p2.filterCards(["Condesa", "Drácula", "Perro"])[:]
+
+        assert [c.name for c in finalCards] == ["Condesa", "Drácula"]
+
+def test_findPlayerIdWithCards_success(app, dataSuspect):
+
+    with db_session:
+        g1 = Game[1]
+
+        response = g1.findPlayerIdWithCards(["Condesa", "Drácula", "Panteón"], fromPlayerId=1)
+
+        assert response["playerId"] == 2
+        assert response["cards"] == ["Condesa", "Drácula"]
+
+def test_findPlayerIdWithCards_successSecondPlayer(app, dataSuspect):
+
+    with db_session:
+        g1 = Game[1]
+
+        response = g1.findPlayerIdWithCards(["Conde", "Momia", "Panteón"], fromPlayerId=1)
+
+        assert response["playerId"] == 3
+        assert response["cards"] == ["Conde"]
+
+
+def test_findPlayerIdWithCards_noCards(app, dataSuspect):
+
+    with db_session:
+        g1 = Game[1]
+
+        response = g1.findPlayerIdWithCards(["Gato", "Perro", "Hola"], fromPlayerId=1)
+
+        assert response is None
+
+def test_findPlayerIdWithCards_imLastPlayer(app, dataSuspect):
+
+    with db_session:
+        g1 = Game[1]
+
+        response = g1.findPlayerIdWithCards(["Panteón", "Condesa", "Doncella"], fromPlayerId=4)
+
+        assert response["playerId"] == 1
+        assert response["cards"] == ["Doncella"]
+
+def test_findPlayerIdWithCards_fromFirstPlayer(app, dataSuspect):
+
+    with db_session:
+        g1 = Game[1]
+
+        response = g1.findPlayerIdWithCards(["Panteón", "Condesa", "Doncella"], fromPlayerId=1)
+
+        assert response["playerId"] == 2
+        assert response["cards"] == ["Condesa"]
